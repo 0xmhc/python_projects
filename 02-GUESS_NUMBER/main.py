15508c40c69f7bb201a66f74f23bb09661c23149
0xmhc <Mouhcine>
2024-09-29 18:05:42 +0100

    add function on file main.py

--- 02-GUESS_NUMBER/main.py
+++ 02-GUESS_NUMBER/main.py
@@ -10,4 +10,23 @@ def guess(x) :
         elif guess < random_number :
             print("Sorry, Guess again. Too low")
     print(f"Great, You Win, The number is {guess}")
-guess(5)
+
+def guess_computer(x) :
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c' :
+        if low != high :
+            guess = random.randint(low,high)
+        else :
+            guess = low
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C): ').lower()
+        if feedback == "h" :
+            high = guess - 1
+        elif feedback == "l" :
+            low = guess + 1
+    print(f"Yeah, The computer guessed your number, {guess}, correctly!")
+    
+
+guess_computer(500)
+# guess(5)
